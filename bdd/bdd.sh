0fd2077a78d21b0379d82a41f838a5f57a2d0d5d
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2019-10-14 13:56:55 +0200

    feat: port changes from upstream for CJXD release v4
    Signed-off-by: Cosmin Cojocar <cosmin.cojocar@gmx.ch>

--- bdd/bdd.sh
+++ bdd/bdd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 set -x
 
 if [ $# -ne 2 ]; then
@@ -11,11 +11,7 @@ DST_PATH=$2
 
 export GH_USERNAME="jenkins-x-bot-test"
 export GH_EMAIL="jenkins-x@googlegroups.com"
-export GH_OWNER="cb-kubecd"
-
-export GH_CREDS_PSW="$(jx step credential -s jenkins-x-bot-test-github)"
-export JENKINS_CREDS_PSW="$(jx step credential -s  test-jenkins-user)"
-export GKE_SA="$(jx step credential -k bdd-credentials.json -s bdd-secret -f sa.json)"
+export GH_OWNER="jenkins-x-bot-test"
 
 # fix broken `BUILD_NUMBER` env var
 export BUILD_NUMBER="$BUILD_ID"
@@ -28,20 +24,21 @@ mkdir -p $JX_HOME
 jx --version
 jx step git credentials
 
+# setup GCP service account
 gcloud auth activate-service-account --key-file $GKE_SA
 
-# lets setup git 
+# setup git 
 git config --global --add user.name JenkinsXBot
 git config --global --add user.email jenkins-x@googlegroups.com
 
 echo "running the BDD tests with JX_HOME = $JX_HOME"
 
 # setup jx boot parameters
-export JX_VALUE_ADMINUSER_PASSWORD="$JENKINS_CREDS_PSW"
+export JX_VALUE_ADMINUSER_PASSWORD="$JENKINS_PASSWORD"
 export JX_VALUE_PIPELINEUSER_USERNAME="$GH_USERNAME"
 export JX_VALUE_PIPELINEUSER_EMAIL="$GH_EMAIL"
-export JX_VALUE_PIPELINEUSER_TOKEN="$GH_CREDS_PSW"
-export JX_VALUE_PROW_HMACTOKEN="$GH_CREDS_PSW"
+export JX_VALUE_PIPELINEUSER_TOKEN="$GH_ACCESS_TOKEN"
+export JX_VALUE_PROW_HMACTOKEN="$GH_ACCESS_TOKEN"
 
 # TODO temporary hack until the batch mode in jx is fixed...
 export JX_BATCH_MODE="true"
@@ -52,6 +49,20 @@ cp -r `ls -A | grep -v "${DST_PATH}"` $DST_PATH
 cp $SRC_PATH/jx-requirements.yml $DST_PATH
 cp $SRC_PATH/parameters.yaml $DST_PATH/env
 cd $DST_PATH
+
+# Rotate the domain to avoid cert-manager API rate limit
+if [[ "${DOMAIN_ROTATION}" == "true" ]]; then
+    SHARD=$(date +"%l" | xargs)
+    DOMAIN="${DOMAIN_PREFIX}${SHARD}${DOMAIN_SUFFIX}"
+    if [[ -z "${DOMAIN}" ]]; then
+        echo "Domain rotation enabled. Please set DOMAIN_PREFIX and DOMAIN_SUFFIX environment variables" 
+        exit -1
+    fi
+    echo "Using domain: ${DOMAIN}"
+    sed -i "/^ *ingress:/,/^ *[^:]*:/s/domain: .*/domain: ${DOMAIN}/" jx-requirements.yml
+fi
+echo "Using jx-requirements.yml"
+cat jx-requirements.yml
 
 # TODO hack until we fix boot to do this too!
 helm init --client-only
@@ -65,8 +76,8 @@ jx step bdd \
     --git-provider=github \
     --git-username $GH_USERNAME \
     --git-owner $GH_OWNER \
-    --git-api-token $GH_CREDS_PSW \
-    --default-admin-password $JENKINS_CREDS_PSW \
+    --git-api-token $GH_ACCESS_TOKEN \
+    --default-admin-password $JENKINS_PASSWORD \
     --no-delete-app \
     --no-delete-repo \
     --tests install \
